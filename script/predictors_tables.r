04aaf4d3047997e71f076f39897e642feda8ab93
sagesteppe <reedbenkendorf27@gmail.com>
2022-11-19 17:59:13 -0700

    updated some text and style

---
+++ script/predictors_tables.r
@@ -0,0 +1,30 @@
+
+Layer                   Description                            Source
+----    ----------------------------------------------  ---------------------
+  1.             Mean annual cloudiness - MODIS            Wilson et al. 2016
+2.           Cloudiness seasonality 1 - MODIS            Wilson et al. 2016 
+3.           Cloudiness seasonality 2 - MODIS            Wilson et al. 2016 
+4.           Cloudiness seasonality 3 - MODIS            Wilson et al. 2016
+5.          Beginning of the frost-free period               Wang et al.
+6.             Climatic moisture deficit                     Wang et al.
+7.             Degree-days above 5C from                     Wang et al.
+8.             Mean annual precipitation                     Wang et al.
+9.          Mean annual precipitation as snow                Wang et al.
+10.             Temperature seasonality                      Wang et al.
+11.       2015 Percent Grass/Herbaceous cover - MODIS         (MOD44B)
+12.      2015 Percent Tree cover from Landsat 7/8              (GLCF)
+13.       Soil probability of bedrock (R Horizon)             SoilGrids
+14.         Soil organic carbon (Tonnes / ha)                 SoilGrids
+15.              Surface soil pH in H2O                       SoilGrids
+16.             Surface soil percent sand                     SoilGrids
+17.                    Soil USDA class                        SoilGrids
+18.                Topographic elevation                    EarthEnv DEM
+19.          Topographic elevation, moving window           EarthEnv DEM
+20.            Topographic percent slope                    EarthEnv DEM 
+21.            Topographic wetness index                    EarthEnv DEM 
+22.        Topographic aspect from EarthEnv DEM
+23.      Annual potential solar radiation computed             r.sun.
+24.       Estimated actual (w/-cloud) solar radiation
+25.       Log-transformed distance to surface water   Global Surface Water Explorer
+26.             Percent surface water from            Global Surface Water Explorer 
+----    ----------------------------------------------  ---------------------
