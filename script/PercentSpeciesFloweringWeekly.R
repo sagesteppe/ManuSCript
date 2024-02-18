cf5167f694ecf2a92ae34007b17af07ebbc16f96
sagesteppe <reedbenkendorf27@gmail.com>
2024-02-18 13:35:56 -0700

    barplot with error bars supposedly of number of species flowering within ci

---
+++ script/PercentSpeciesFloweringWeekly.R
@@ -0,0 +1,72 @@
+setwd('~/Documents/ManuSCript/script')
+
+library(tidyverse)
+
+weibs <- bind_rows(
+  read.csv('../data/weibull_estimates.csv') %>% 
+  mutate(Estimate = 'SubalpineOnly'), 
+  read.csv('../data/weibull_estimates_mixed_montane.csv') %>% 
+  mutate(Estimate = 'SubalpineMontane')
+) %>% 
+  arrange(taxon) %>% 
+  filter(metric %in% c('tenth', 'ninety')) %>% 
+  select(event, taxon, doy = DOY)
+
+weibs <- split(weibs, f = list( weibs$taxon, weibs$event))
+weibs <- lapply(weibs, function(x){data.frame(
+  doy = as.matrix(
+    seq(x$doy[1], x$doy[2])))})  %>% 
+  bind_rows(.id = 'taxon')
+
+bees <- read.csv('../data/Bombus_queen_observations_2015.csv') %>% 
+  mutate(species = gsub('[.].*$', '', species)) %>% 
+  select(year, week, species) %>% 
+  group_by(species, week) %>% 
+  tally() %>% 
+  filter(! species %in% c('centralis', 'melanopygus', 'kirbiellus', 'unknown'))
+
+#ggplot(bees, aes(x = n, y = species)) + 
+#  geom_density_ridges()
+
+weeks <- data.frame(
+  week = rep(1:19, each = 7), 
+  doy = 
+    c(130:136, 137:143, 144:150, 151:157, 158:164, 165:171, 172:178, 179:185, 186:192, 
+      193:199, 200:206, 207:213, 214:220, 221:227, 228:234, 235:241, 242:248, 249:255, 256:262)
+    )
+
+test <- left_join(weibs, weeks) %>% 
+  distinct(taxon, week) %>% 
+  filter(week %in% 3:11) %>% 
+  separate(taxon,  c('taxon', 'estimate'), '[.]')
+
+wkkly <- test %>% 
+  group_by(week) %>% 
+  filter(estimate == 'estimate') %>% 
+  tally()
+wkkly$prop <- wkkly$n / nrow(distinct(test, taxon))
+
+wkklyLow <- test %>% 
+  group_by(week) %>% 
+  filter(estimate == 'low_ci') %>% 
+  tally()
+wkklyLow$prop <- wkklyLow$n / nrow(distinct(test, taxon))
+
+wkklyHigh <- test %>% 
+  group_by(week) %>% 
+  filter(estimate == 'high_ci') %>% 
+  tally()
+wkklyHigh$prop <- wkklyHigh$n / nrow(distinct(test, taxon))
+
+barCenters <- barplot(wkkly$prop, horiz = F, names.arg = 3:11, ylim = c(0,1), 
+        yaxt = "n", xlab = 'Week', ylab = 'Proportion', 
+        main = 'Proportion of the 379 phenology\nmodelled species flowering by week')
+axis(side = 2, las = 2)
+
+segments(x0 = barCenters, y0 = wkklyLow$prop, 
+         x1 = barCenters, y1= wkklyHigh$prop)
+arrows(x0 = barCenters, y0 = wkklyLow$prop, 
+       x1 = barCenters, y1= wkklyHigh$prop, lwd = 1.5, angle = 90,
+       code = 3, length = 0.02)
+
+rm(weeks)
